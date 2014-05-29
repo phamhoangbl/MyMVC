3a968f7ad55e76b973680cf03821c7a07ddb66e8
John Hoang <john.hoang.contact@gmail.com>
2014-05-30 01:42:57 +0700

    Add Employee Details page.

---
+++ MyMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMVC.Models;
+
+namespace MyMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult Index()
+        {
+            EmployeeContext context = new EmployeeContext();
+            List<Employee> employees = context.Employees.ToList();
+            return View(employees);
+        }
+
+        public ActionResult Details(int id)
+        {
+            //Employee employee = new Employee() { EmployeeId = 101, Gender = "Male", City = "HCM", Name = "John"};
+            EmployeeContext context = new EmployeeContext();
+            Employee employee = context.Employees.Single(emp => emp.EmployeeId == id);
+            return View(employee);
+        }
+    }
+}
--- MyMVC/Controllers/HomeController.cs
+++ MyMVC/Controllers/HomeController.cs
@@ -11,12 +11,5 @@ namespace MyMVC.Controllers
     {
         //
         // GET: /Home/
-        public ActionResult Details(int id)
-        {
-            //Employee employee = new Employee() { EmployeeId = 101, Gender = "Male", City = "HCM", Name = "John"};
-            EmployeeContext context = new EmployeeContext();
-            Employee employee = context.Employees.Single(emp => emp.EmployeeId == id);
-             return View(employee);
-        }
 	}
 }
