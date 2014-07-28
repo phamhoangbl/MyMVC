52db17e2b305ada2ddd4ed758cc2038e9072cbe2
John Hoang <john.hoang.contact@gmail.com>
2014-07-29 00:45:45 +0700

    Using business objects

---
+++ MyMVC/Controllers/EmployeeBusinessController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace MyMVC.Controllers
+{
+    public class EmployeeBusinessController : Controller
+    {
+        //
+        // GET: /EmployeeBusiness/
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer emp = new EmployeeBusinessLayer();
+            List<Employee> listEmployee = emp.Employees.ToList();
+            return View(listEmployee);
+        }
+	}
+}
